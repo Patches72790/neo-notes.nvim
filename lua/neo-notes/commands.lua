ff3177ad18adfe4b0a632ce9d645e1eaab026e64
Patrick <patches72790@gmail.com>
2022-05-26 22:45:44 -0500

    added commands folder

---
+++ lua/neo-notes/commands.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+local create_neo_notes_cmd = function()
+	local NV_NOTES_PATH = os.getenv("NV_NOTES_PATH") or os.getenv("HOME") .. "/Notes"
+	vim.api.nvim_create_user_command("NeoNotes", function()
+		require("telescope.builtin").find_files({ cwd = NV_NOTES_PATH })
+		--vim.api.nvim_open_win(0, true, { relative = "win", row = 3, col = 3, width = 24, height = 24 })
+	end, { desc = "Open neovim notes from default path location" })
+end
+
+M.create_commands = function()
+	create_neo_notes_cmd()
+end
+
+return M
